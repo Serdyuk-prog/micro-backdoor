77d3d3cc7b10f9124fa0189387cdbb70d98ed8c1
Serdyuk-prog <tony.not.gloomy@gmail.com>
2022-11-27 16:03:12 +0700

    done

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const fs = require("fs");
 const catchAsync = require("./utils/catchAsync");
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.set("layout", "layout.ejs");
 app.engine("ejs", ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(expressLayouts);
 
-const testFolder = "./files/";
+const sharedFolder = "./files/";
 
 const getList = async (folder) => {
     return await fs.promises.readdir(folder, (err, files) => {
@@ -31,18 +38,61 @@ app.get("/", (req, res) => {
 app.get(
     "/show/*",
     catchAsync(async (req, res) => {
-        const list = await getList(`./files/${req.url.slice(5)}`);
-        res.render("home", { list });
+        let filePath;
+        if (req.url.slice(6) == "") {
+            filePath = `${sharedFolder}${req.url.slice(6)}`;
+        } else {
+            filePath = `${sharedFolder}${req.url.slice(6)}/`;
+        }
+        const fileNames = await getList(filePath);
+        const list = [];
+        for (let name of fileNames) {
+            let type = "dir";
+            if (name.indexOf(".") > -1) {
+                type = "file";
+            }
+            list.push({
+                name: name,
+                type: type,
+            });
+        }
+        res.render("home", { list, filePath });
     })
 );
 
 app.get("/download/*", (req, res) => {
-    let filePath = path.join(__dirname, "/files/cat.jpg");
+    let filePath = path.join(__dirname, `${req.url.slice(10)}`);
     res.download(filePath);
 });
 
 app.post("/new-folder", (req, res) => {
-    res.send(req.body);
+    const dir = sharedFolder + req.body.dir.slice(6);
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+    res.sendStatus(200);
+});
+
+app.post("/delete", (req, res) => {
+    let filePath = path.join(__dirname, req.body.path);
+    if (fs.existsSync(filePath)) {
+        fs.rmSync(filePath, { recursive: true, force: true });
+    }
+    res.sendStatus(200);
+});
+
+app.post("/new-file/*", upload.single("uploadFile"), (req, res) => {
+    let filePath = path.join(__dirname, `${req.url.slice(10)}`);
+    const file = req.file;
+    fs.writeFile(
+        `${filePath}${file.originalname}`,
+        file.buffer,
+        function (err) {
+            if (err) return console.log(err);
+            console.log("file uploaded");
+        }
+    );
+    res.redirect(`/show/`);
 });
 
 app.use((err, req, res, next) => {
